210ede4508eebe4bf6ea18ffd5bf39dc173fc95c
Suryakanta Dash <sdash8273@gmail.com>
2023-10-30 12:29:36 +0530

    updated

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -4,10 +4,10 @@ import { HiLocationMarker } from 'react-icons/hi'
 const SearchBar = () => {
   return (
     <div className="flexCenter search-bar">
-    <HiLocationMarker color="var(--blue)" size={25} />
+    <HiLocationMarker color="var(--blue)" size={25} /> 
     <input type="text" />
     <button className="button">Search</button>
-  </div>
+    </div>
   )
 }
 
